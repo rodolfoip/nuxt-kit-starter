20c8d77c7182c3fcf1ad362d5f575ff9f77a80b1
Rodolfo <rodolfo.pereirailce@gmail.com>
2018-09-13 09:52:24 -0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'vrau',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: '{{escape description }}'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  css: [
+    '@/assets/styles/main.scss'
+  ],
+  plugins: ['~plugins/bootstrap.js'],
+  loading: {color: '#3B8070'},
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, {isDev, isClient}) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    vendor: ['jquery', 'bootstrap'],
+    plugins: [
+      new webpack.ProvidePlugin({
+        '$': 'jquery',
+        'jquery': 'jquery',
+        'jQuery': 'jquery'
+      })
+    ]
+  }
+}
+
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div>
+      <div class="text-center">
+        <app-logo/>
+        <h1 class="title">
+          haha
+        </h1>
+        <h2 class="subtitle">
+          asgagds
+        </h2>
+      </div>
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab"
+                                                  data-toggle="tab">Home</a></li>
+        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a>
+        </li>
+        <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a>
+        </li>
+      </ul>
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="home">Home text...</div>
+        <div role="tabpanel" class="tab-pane" id="profile">Profile text...</div>
+        <div role="tabpanel" class="tab-pane" id="messages">Messages text...</div>
+        <div role="tabpanel" class="tab-pane" id="settings">Settings text...</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import AppLogo from '~/components/AppLogo.vue'
+
+  export default {
+    components: {
+      AppLogo
+    }
+  }
+</script>
