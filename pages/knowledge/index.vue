aad9b07f257115ff0c4014ea9c5419ce047d88ae
Rodolfo <rodolfo.pereirailce@gmail.com>
2018-10-04 10:49:40 -0300

    sfa

--- pages/knowledge/index.vue
+++ pages/knowledge/index.vue
@@ -2,7 +2,21 @@
   <main class="knowledge">
     <section class="section-default">
       <div class="container">
-        Conhecimentos
+        <div class="row">
+          <div class="card-columns">
+            <div class="card" v-for="(n, index) in 8" :key="index">
+              <img class="card-img-top" src="https://via.placeholder.com/180X100" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title {{n}}</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </main>
